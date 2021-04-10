b06bfef03f24aa4ff7154338bcb5fddcb6ff13f2
Gaëtan Mirmand <gmirmand@webqam.fr>
2021-04-10 17:01:59 +0200

    :wrench: Various configuration

---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+// firebase init - add your own config here
+const firebaseConfig = {
+  apiKey: "AIzaSyA9XYXCI9aEnisoLv_152AYHFJu63Q-jTE",
+  authDomain: "ggstreaming-d7c7c.firebaseapp.com",
+  projectId: "ggstreaming-d7c7c",
+  storageBucket: "ggstreaming-d7c7c.appspot.com",
+  messagingSenderId: "251639121924",
+  appId: "1:251639121924:web:fbc8c88d56a9685bc98d51",
+  measurementId: "G-SN8GZKWWJ6",
+};
+firebase.initializeApp(firebaseConfig);
+
+// utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+// collection references
+const usersCollection = db.collection("users");
+const postsCollection = db.collection("posts");
+const commentsCollection = db.collection("comments");
+const likesCollection = db.collection("likes");
+
+// export utils/refs
+export {
+  db,
+  auth,
+  usersCollection,
+  postsCollection,
+  commentsCollection,
+  likesCollection,
+};
