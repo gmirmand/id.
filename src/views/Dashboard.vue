f96ec9fd96bd6fbde1e850621cee4941eb997c13
Gaëtan Mirmand <gmirmand@webqam.fr>
2021-04-10 21:02:20 +0200

    :sparkles: Edit dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,25 @@
 <template>
-
+  <div id="dashboard">
+    <section>
+      <div class="col1">
+        <div class="profile">
+          <h5>{{ userProfile.name }}</h5>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
-  name: "Dashboard"
+  components: {},
+  computed: {
+    ...mapState(["userProfile", "posts"]),
+  },
+  methods: {},
 };
 </script>
 
-<style scoped>
-
-</style>
+<style lang="scss" scoped></style>
