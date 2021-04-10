7346e13265e27c8c670bc0c0c45715a932ae1245
Gaëtan Mirmand <gmirmand@webqam.fr>
2021-04-10 17:05:56 +0200

    :lock: Add identification routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
+import { auth } from "../firebase";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,9 @@ const routes = [
     path: "/",
     name: "Dashboard",
     component: Dashboard,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/login",
@@ -21,8 +25,21 @@ const routes = [
     name: "settings",
     component: () =>
       import(/* webpackChunkName: "settings" */ "../views/Settings.vue"),
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
+
+  if (requiresAuth && !auth.currentUser) {
+    next("/login");
+  } else {
+    next();
+  }
+});
 
 const router = new VueRouter({
   mode: "history",
