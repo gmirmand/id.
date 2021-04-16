99d262e657faeb52f2926d36fd5424b277bccf1e
gmirmand <gmirmand@gmail.com>
2021-04-17 00:13:56 +0200

    💄 Add logo & dark-mode

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,18 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      light: {
+        primary: "#500472",
+        secondary: "#79cbb8",
+      },
+      dark: {
+        primary: "#79cbb8",
+        secondary: "#500472",
+      },
+    },
+  },
+});
