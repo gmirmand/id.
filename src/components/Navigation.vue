eba581de84d8e7496d7bef44c2fdfa0367217f62
gmirmand <gmirmand@gmail.com>
2021-04-17 01:55:02 +0200

    ✨ Readd navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,29 +1,52 @@
 <template>
   <v-navigation-drawer :value="drawer" @input="toggle" absolute temporary>
-    <v-list nav dense>
-      <v-list-item-group
-        v-model="group"
-        active-class="deep-purple--text text--accent-4"
-      >
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Home</v-list-item-title>
-        </v-list-item>
+    <v-list>
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+        </v-list-item-avatar>
+      </v-list-item>
 
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon>mdi-account</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Account</v-list-item-title>
-        </v-list-item>
-      </v-list-item-group>
+      <v-list-item link>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            {{ userProfile.name }}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            {{ userProfile.email }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list dense nav>
+      <v-list-item to="/">
+        <v-list-item-icon>
+          <v-icon>mdi-animation-play</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title> Tableau de bord</v-list-item-title>
+      </v-list-item>
+      <v-list-item link to="/settings">
+        <v-list-item-icon>
+          <v-icon>mdi-cogs</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title> Paramètres</v-list-item-title>
+      </v-list-item>
+      <v-list-item link @click="logout()">
+        <v-list-item-icon>
+          <v-icon>mdi-logout</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title> Se déconnecter</v-list-item-title>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "Navigation",
   data() {
@@ -41,6 +64,12 @@ export default {
     toggle(e) {
       !e && this.$emit("close");
     },
+    logout() {
+      this.$store.dispatch("logout");
+    },
+  },
+  computed: {
+    ...mapState(["userProfile"]),
   },
 };
 </script>
