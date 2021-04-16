063de600bf225ece5a0a6eb4634aaf376fe34cfe
gmirmand <gmirmand@gmail.com>
2021-04-17 01:48:37 +0200

    ♻ Extract TopBar

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,9 @@
 <template>
   <v-app>
     <v-main class="app">
+      <Navigation :drawer="drawer" @close="drawer = !drawer" />
+      <TopBar @toggle="drawer = !drawer" />
       <v-container fluid class="pa-0 app__container d-flex flex-column">
-        <v-app-bar ligth class="flex-grow-0">
-          <v-container fluid class="d-flex justify-space-between">
-            <router-link to="/">
-              <v-toolbar-title>
-                <v-img
-                  :src="this.$vuetify.theme.dark ? logoDark : logo"
-                  max-width="50"
-                  max-height="50"
-                />
-              </v-toolbar-title>
-            </router-link>
-            <div>
-              <v-tooltip v-if="!$vuetify.theme.dark" bottom>
-                <template v-slot:activator="{ on }">
-                  <v-btn v-on="on" small fab @click="darkMode">
-                    <v-icon class="mr-1">mdi-moon-waxing-crescent</v-icon>
-                  </v-btn>
-                </template>
-                <span>Dark Mode On</span>
-              </v-tooltip>
-
-              <v-tooltip v-else bottom>
-                <template v-slot:activator="{ on }">
-                  <v-btn v-on="on" small fab @click="darkMode">
-                    <v-icon>mdi-white-balance-sunny</v-icon>
-                  </v-btn>
-                </template>
-                <span>Dark Mode Off</span>
-              </v-tooltip>
-            </div>
-          </v-container>
-        </v-app-bar>
         <v-container>
           <router-view />
         </v-container>
@@ -43,29 +13,19 @@
 </template>
 
 <script>
-import logo from "@/assets/img/svg/logo.svg";
-import logoDark from "@/assets/img/svg/logo-dark.svg";
-
 import { mapState } from "vuex";
+import Navigation from "@/components/Navigation";
+import TopBar from "@/components/TopBar";
 
 export default {
   data() {
     return {
-      logo,
-      logoDark,
+      drawer: false,
     };
   },
-  components: {},
+  components: { TopBar, Navigation },
   computed: {
     ...mapState(["userProfile"]),
-    showNav() {
-      return this.userProfile && Object.keys(this.userProfile).length > 0;
-    },
-  },
-  methods: {
-    darkMode() {
-      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
-    },
   },
 };
 </script>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app-bar ligth class="flex-grow-0">
+    <v-container fluid class="d-flex justify-space-between">
+      <v-app-bar-nav-icon @click="toggle"></v-app-bar-nav-icon>
+      <router-link to="/">
+        <v-toolbar-title>
+          <v-img
+            :src="this.$vuetify.theme.dark ? logoDark : logo"
+            max-width="50"
+            max-height="50"
+          />
+        </v-toolbar-title>
+      </router-link>
+      <div>
+        <v-tooltip v-if="!$vuetify.theme.dark" bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" small fab @click="darkMode">
+              <v-icon class="mr-1">mdi-moon-waxing-crescent</v-icon>
+            </v-btn>
+          </template>
+          <span>Dark Mode On</span>
+        </v-tooltip>
+
+        <v-tooltip v-else bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" small fab @click="darkMode">
+              <v-icon>mdi-white-balance-sunny</v-icon>
+            </v-btn>
+          </template>
+          <span>Dark Mode Off</span>
+        </v-tooltip>
+      </div>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+import logo from "@/assets/img/svg/logo.svg";
+import logoDark from "@/assets/img/svg/logo-dark.svg";
+export default {
+  name: "TopBar",
+  data() {
+    return {
+      logo,
+      logoDark,
+    };
+  },
+  methods: {
+    toggle() {
+      this.$emit("toggle");
+    },
+    darkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+    showNav() {
+      return this.userProfile && Object.keys(this.userProfile).length > 0;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
