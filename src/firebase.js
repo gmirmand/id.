f3e7c3c8e41b23070ba47d3224334e547c2189f5
gmirmand <gmirmand@gmail.com>
2021-04-18 00:02:20 +0200

    :ok_hand: Review firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -20,16 +20,6 @@ const auth = firebase.auth();
 
 // collection references
 const usersCollection = db.collection("users");
-const postsCollection = db.collection("posts");
-const commentsCollection = db.collection("comments");
-const likesCollection = db.collection("likes");
 
 // export utils/refs
-export {
-  db,
-  auth,
-  usersCollection,
-  postsCollection,
-  commentsCollection,
-  likesCollection,
-};
+export { db, auth, usersCollection };
