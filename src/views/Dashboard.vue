07c576272103b45a2c892f4da003a698740cf7d7
Gaëtan Mirmand <gmirmand@webqam.fr>
2021-05-15 21:58:53 +0200

    :sparkles: Add platform button

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,13 @@
 <template>
   <section class="dashboard">
     <DashboardList personal class="mb-6" />
+
+    <div class="d-flex justify-center mt-2 mb-6">
+      <v-btn class="mx-2" fab elevation="6">
+        <v-icon dark> mdi-plus </v-icon>
+      </v-btn>
+    </div>
+
     <DashboardList shared />
   </section>
 </template>
